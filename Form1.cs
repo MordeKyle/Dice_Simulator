f236ddb18729441dcdd04c32dafff04d62209fd3
Kyle McBride <Kyle@MordeKyle.com>
2014-03-06 12:17:46 -0600

    Version: 1.03 Accomplished Robin
    02/06/2014 @ 11:00
    •changed header comment to /* instead of //. Looks cleaner.

--- Form1.cs
+++ Form1.cs
@@ -7,12 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//Program: Dice Simulator
-//Author: Kyle McBride A02609917
-//Date: 02/28/2014
-//Description: Application that simulates rolling a pair of dice. When the user clicks a button, the application 
-//             will generate two random numbers, each in the range of 1 through 6, to represent the value of the dice.
-//                                        ***SEE CHANGELOG IN SOLUTION DIRECTORY***
+/*Program: Dice Simulator
+  Author: Kyle McBride A02609917
+  Date: 02/28/2014
+  Description: Application that simulates rolling a pair of dice. When the user clicks a button, the application 
+               will generate two random numbers, each in the range of 1 through 6, to represent the value of the dice.
+                                          ***SEE CHANGELOG IN SOLUTION DIRECTORY***                                  */
 
 namespace Dice_Simulator
 {
